b8abfe8c54af1fdb161dbb6c5fe4ea1aa339e1c2
Paul Eipper <paul@nkey.com.br>
2012-02-03 22:53:24 -0200

    Fast load PE resource directories

--- innounpy.py
+++ innounpy.py
@@ -1,6 +1,7 @@
 import pefile
 import struct
 from collections import OrderedDict
+from pprint import pprint
 
 # magic numbers
 SetupLdrOffsetTableResID = 11111
@@ -9,7 +10,9 @@ SetupLdrOffsetTableResID = 11111
 filename = 'setup_tyrian_2000.exe'
 
 # parse executable resources to find TSetupLdrOffsetTable offset
-pe = pefile.PE(filename)
+pe = pefile.PE(filename, fast_load=True)
+pe.parse_data_directories(directories=[pefile.DIRECTORY_ENTRY['IMAGE_DIRECTORY_ENTRY_RESOURCE']])
+
 rt_rcdata_index = [entry.id for entry in pe.DIRECTORY_ENTRY_RESOURCE.entries].index(pefile.RESOURCE_TYPE['RT_RCDATA'])
 rt_rcdata_directory = pe.DIRECTORY_ENTRY_RESOURCE.entries[rt_rcdata_index]
 resource = [entry for entry in rt_rcdata_directory.directory.entries if entry.id == SetupLdrOffsetTableResID][0]
@@ -18,11 +21,13 @@ resource = resource.directory.entries[0]
 data_rva = resource.data.struct.OffsetToData
 size = resource.data.struct.Size
 TSetupLdrOffsetTableData = pe.get_memory_mapped_image()[data_rva:data_rva+size]
+pe.close()
 
 # extract TSetupLdrOffsetTable fields
 keys = ['ID', 'Version', 'TotalSize', 'OffsetEXE', 'UncompressedSizeEXE', 'CRCEXE', 'Offset0', 'Offset1', 'TableCRC']
 values = struct.unpack('<12s8L', TSetupLdrOffsetTableData)
 TSetupLdrOffsetTable = OrderedDict(zip(keys, values))
 
-import pprint
-pprint.pprint(TSetupLdrOffsetTable.items())
+print('TSetupLdrOffsetTable:')
+pprint(TSetupLdrOffsetTable.items())
+
